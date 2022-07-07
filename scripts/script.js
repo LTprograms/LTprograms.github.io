e76d43b1358bcf031e19a6f8a54d89caa48d4875
LTprograms <105554066+LTprograms@users.noreply.github.com>
2022-07-07 01:19:19 -0500

    version 2

--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,35 @@ var buenas=0, malas=0, total = 0;
 function subir() {
     var txt = document.getElementById("texto");
     var cbuenas = document.getElementById("contB");
-    var ctotal = document.getElementById("contT");
-    puntaje+=20;
-    buenas++;
-    total++;
-    txt.textContent = "Puntaje: "+puntaje;
-    cbuenas.textContent = "Buenas: "+buenas;
-    ctotal.textContent = "Total: "+total;
+    var ctotal = document.getElementById("contT"); 
+    if (total<100) {
+        puntaje+=20;
+        buenas++;
+        total++;
+        txt.textContent = "Puntaje: "+puntaje;
+        cbuenas.textContent = "Buenas: "+buenas;
+        ctotal.textContent = "Total: "+total;
+    }
 }
 function bajar() {
     var txt = document.getElementById("texto");
     var cmalas = document.getElementById("contM");
     var ctotal = document.getElementById("contT");
-    puntaje-=1.125;
-    txt.textContent = "Puntaje: "+puntaje;
-    malas++;
-    total++;
-    cmalas.textContent = "Malas: "+malas;
-    ctotal.textContent = "Total: "+total;
+    if (total<100) {
+        puntaje-=1.125;
+        txt.textContent = "Puntaje: "+puntaje;
+        malas++;
+        total++;
+        cmalas.textContent = "Malas: "+malas;
+        ctotal.textContent = "Total: "+total;
+    }
+    
 }
 
+var b = document.getElementById("bad");
 var g = document.getElementById("good");
 g.onclick = subir;
-var b = document.getElementById("bad");
 b.onclick = bajar;
+
+    
+
