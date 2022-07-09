99bc9ee5bb7578cfdc57a4b842b078f1ec1598b4
LTprograms <105554066+LTprograms@users.noreply.github.com>
2022-07-09 12:30:16 -0500

    version 3.0

---
+++ scripts/VillaScript.js
@@ -0,0 +1,35 @@
+var puntaje = 0;
+var buenas=0, malas=0, total = 0;
+
+function subir() {
+    var txt = document.getElementById("texto");
+    var cbuenas = document.getElementById("contB");
+    var ctotal = document.getElementById("contT"); 
+    if (total<100) {
+        puntaje+=10;
+        buenas++;
+        total++;
+        txt.textContent = "Puntaje: "+puntaje;
+        cbuenas.textContent = "Buenas: "+buenas;
+        ctotal.textContent = "Total: "+total;
+    }
+}
+function bajar() {
+    var txt = document.getElementById("texto");
+    var cmalas = document.getElementById("contM");
+    var ctotal = document.getElementById("contT");
+    if (total<100) {
+        puntaje-=0.25219265;
+        txt.textContent = "Puntaje: "+puntaje;
+        malas++;
+        total++;
+        cmalas.textContent = "Malas: "+malas;
+        ctotal.textContent = "Total: "+total;
+    }
+    
+}
+
+var b = document.getElementById("bad");
+var g = document.getElementById("good");
+g.onclick = subir;
+b.onclick = bajar;
